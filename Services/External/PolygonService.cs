0801a7ead52143a08cbbc2390bd76b4575501c72
Israel <israelshlomo855@gmail.com>
2025-05-02 11:52:08 +0300

    FINAL PROJECT :)

--- Services/External/PolygonService.cs
+++ Services/External/PolygonService.cs
@@ -1,10 +1,16 @@
-﻿using System.Text.Json;
+﻿// שימוש ב־System.Text.Json לעבודה עם JSON
+using System.Text.Json;
+
 
 public class PolygonService
 {
+
+    // לקוח HTTP שמשמש לשליחת בקשות API
     private readonly HttpClient _httpClient;
     private readonly string _apiKey = "B3oUsO0EkvpF9xzR8vq2ob4XDP4zcx80\r\n";
 
+
+    //DEPENTENCY INJECTION
     public PolygonService(HttpClient httpClient)
     {
         _httpClient = httpClient;
@@ -18,8 +24,13 @@ public class PolygonService
             var response = await _httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
 
+            // קריאת תוכן התשובה כ־string
             var json = await response.Content.ReadAsStringAsync();
+            // ואז ממיר אותו ל JSON
             var doc = JsonDocument.Parse(json);
+
+
+            // שליפת המחיר האחרון מתוך ה־JSON
 
             return doc.RootElement.GetProperty("results")[0].GetProperty("c").GetDouble();
         }
